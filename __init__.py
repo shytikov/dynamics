eea40ade2f21e3b31daaf771d74ff2618a5b3283
Alexey Shytikov <alexey.shytikov@crmkonsulterna.se>
2019-10-07 16:09:55 +0200

    Creating dataframe accessor

--- __init__.py
+++ __init__.py
@@ -1,5 +1,14 @@
-from dynamics.objects import Connection
+# flake8: noqa
+
+import pandas
+import numpy
+from dynamics.objects.Connection import Connection
 
 
-def connect(resource: str, login: str, password: str):
-    return Connection(resource, login, password)
+@pandas.api.extensions.register_dataframe_accessor("dynamics")
+class DynamicsAccessor:
+    def __init__(self, obj: pandas.DataFrame) -> None:
+        self.data = obj
+
+    def connect(self, resource: str, login: str, password: str):
+        return Connection(self.data, resource, login, password)
